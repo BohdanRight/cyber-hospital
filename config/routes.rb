20da64404a98d8decc53f4940dac36b5cf7e6811
Yaroslav Solokhudinov <yaroslav.solokhudinov@gmail.com>
2019-12-22 23:03:33 +0200

    1 - Authorization

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :certificates
+
+  resources :users, only: [:new, :create]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  get 'welcome', to: 'sessions#welcome'
+  get 'authorized', to: 'sessions#page_requires_login'
+
+  resources :certificates, path: 'certificates'
   resources :receivings, path: 'receivings'
-  resources :doctors
+  resources :doctors, path: 'doctors'
   resources :patients, path: 'patients'
   root 'doctors#index'
 
