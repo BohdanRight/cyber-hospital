85a5096dc8c93c6a8371e996e97b56294fa26317
Bohdan Riabokliach <bohdan631@gmail.com>
2019-12-23 04:58:30 +0200

    refactor controllers, refactor views

--- config/routes.rb
+++ config/routes.rb
@@ -12,13 +12,5 @@ Rails.application.routes.draw do
   resources :patients, path: 'patients'
   root 'doctors#index'
 
-  #root to: 'mmonitors#index'
-  #devise_for :users
-  #resources :mmonitors, path: 'monitor'
-  #resources :mbrands, path: 'brand'
-  #resources :mtypes, path: 'type'
-  #resources :mdiagonals, path: 'diagonal'
-  #resources :mresolutions, path: 'resolution'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
