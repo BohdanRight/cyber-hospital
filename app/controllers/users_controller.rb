3ad9780fa79c281f0891dda42f63f53b07f07247
Yaroslav Solokhudinov <yaroslav.solokhudinov@gmail.com>
2019-12-23 04:54:14 +0200

    1 - Authorization2.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 
-  skip_before_action :authorized, only: [:new, :create]
+  skip_before_action :authorized, only: [:new, :create, :delete]
 
   def new
     @user = User.new
@@ -10,5 +10,11 @@ class UsersController < ApplicationController
     @user = User.create(params.require(:user).permit(:username, :password))
     session[:user_id] = @user.id
     redirect_to '/welcome'
+  end
+
+  def delete
+    # Remove the user id from the session
+    session[:current_user] = nil
+    current_user.username=nil
   end
 end
